ef6b5cb3ed2bd2d6b852502792f2f65b06d7897a
gerakul <gerasimovku@gmail.com>
2018-08-06 23:08:48 -0400

    app for debug

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Gerakul.FastSql.SqlServer;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestAsync().Wait();
+        }
+
+        public static async Task TestAsync()
+        {
+            var context = SqlContextProvider.FromConnectionString(@"data source=db4mulpj2b.database.windows.net;initial catalog=domain0;User ID=SyncServer;Password=A$e194!eW");
+            var users = context.CreateSimple("select top 10 * from [User]")
+                .ExecuteQueryAnonymousAsync(new { ID = 0, Name = "" }).ToArray().Result;
+
+
+            await context.UsingTransactionAsync(async x =>
+            {
+                var uu = await x.CreateSimple("select top 10 * from [User]")
+                .ExecuteQueryAnonymousAsync(new { ID = 0, Name = "" }).ToArray();
+            });
+
+
+        }
+    }
+}
